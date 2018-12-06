a953a0dd5afe634bd34bbc04f64d9bd470bf604c
Handrews0101 <32401789+Handrews0101@users.noreply.github.com>
2018-12-05 23:49:00 -0500

    Create ScreenDriver.java

---
+++ ScreenDriver.java
@@ -0,0 +1,14 @@
+/**
+ * Author: Andrews, Hannah
+ * Description: Here lies a driver for testing the Screen
+ */
+
+public class ScreenDriver {
+
+  public static void testScreen() {
+
+    Screen screen = new Screen("1080", 60, 1);
+    System.out.println(screen);
+  }
+
+}
