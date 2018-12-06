a6ad11cad895c0e8f37e9310e7862838d5aaa5cc
Handrews0101 <32401789+Handrews0101@users.noreply.github.com>
2018-12-05 23:50:37 -0500

    Create TestProductionLine.java

---
+++ TestProductionLine.java
@@ -0,0 +1,7 @@
+public class TestProductionLine {
+
+    public static void main(String[] args){
+        EmployeeInfo employeeInfo = new EmployeeInfo();
+        System.out.println(employeeInfo.toString());
+    }
+}
