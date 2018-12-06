aaa1c123fed627bdb833394349bd88232c451d92
Handrews0101 <32401789+Handrews0101@users.noreply.github.com>
2018-12-05 23:49:57 -0500

    Create ScreenSpec.java

---
+++ ScreenSpec.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Andrews, Hannah
+ * Description: Here lies an interface for Screens
+ */
+
+public interface ScreenSpec {
+
+  public String getResolution();
+
+  public int getRefreshRate();
+
+  public int getResponseTime();
+
+
+}
