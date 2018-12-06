577227ebd1db19bb83b31d3263ef6664ad0ebdb2
Handrews0101 <32401789+Handrews0101@users.noreply.github.com>
2018-12-05 23:31:53 -0500

    Create MultimediaControl.java

---
+++ MultimediaControl.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Andrews, Hannah
+ * Description: Here lies an interface for all Products with Multimedia Control
+ */
+
+public interface MultimediaControl {
+
+  public void play();
+  
+  public void stop();
+  
+  public void previous();
+  
+  public void next();
+  
+}
