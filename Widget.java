3b345aca1c42475df88f64dde0a8600bea44ccb3
Handrews0101 <32401789+Handrews0101@users.noreply.github.com>
2018-12-05 23:51:49 -0500

    Create Widget.java

---
+++ Widget.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Andrews, Hannah
+ * Description: Here lies a class for Widgets, 
+ * it is an extension/subclass of Product
+ */
+
+class Widget extends Product {
+
+  Widget(String name) {
+    super(name);
+  }
+
+}
