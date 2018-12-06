0e98426b8f07bdeeeb8abd6121933995db22cb93
Handrews0101 <32401789+Handrews0101@users.noreply.github.com>
2018-12-05 23:26:26 -0500

    Create MoviePlayer.java

---
+++ MoviePlayer.java
@@ -0,0 +1,44 @@
+/**
+ * Author: Andrews, Hannah
+ * Description: Here likes a class that describes the MoviePlayer
+ * with many many overrides 
+ */
+public class MoviePlayer extends Product implements MultimediaControl {
+
+  private Screen screen;
+  private MonitorType monitorType;
+
+  MoviePlayer(String name, Screen screen, MonitorType monitorType) {
+    super(name);
+    this.screen = screen;
+    this.monitorType = monitorType;
+  }
+
+  @Override
+  public void play() {
+    System.out.println("Playing movie");
+  }
+
+  @Override
+  public void stop() {
+    System.out.println("Stopping movie");
+  }
+
+  @Override
+  public void previous() {
+    System.out.println("Previous movie");
+  }
+
+  @Override
+  public void next() {
+    System.out.println("Next movie");
+  }
+
+  @Override
+  public String toString() {
+    return
+        super.toString() +
+            "Screen : " + screen +
+            "Monitor Type : " + monitorType + "\n";
+  }
+}
